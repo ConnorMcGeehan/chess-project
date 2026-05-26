d347dceaa267468f38406ff3da6fab9d5c6713d8
Connor McGeehan <cmcgeehan2421@gmail.com>
2026-05-26 13:37:58 -0400

    test headers

--- backend/test_game.py
+++ backend/test_game.py
@@ -2,7 +2,31 @@ from game import Game
 
 class TestGame:
 
+    def test_move(self):
+        #TODO
+        pass
+
+    def test_undo_move(self):
+        #TODO
+        pass
+
     def test_legal_moves(self):
-        self.x = Game()
-        assert self.x.legal_moves() == ""
+        #TODO
+        pass
+
+    def test_turn(self):
+        #TODO
+        pass
+
+    def test_mate_in_one(self):
+        #TODO
+        pass
+
+    def test_check_castle(self):
+        #TODO
+        pass
+
+    def test_get_total_moves(self):
+        #TODO
+        pass
 
